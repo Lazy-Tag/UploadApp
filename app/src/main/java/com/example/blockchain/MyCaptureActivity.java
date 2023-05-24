5d66f272bb435437402e27f84ea478396ef1aa97
Lazy-Tag <hbw.lazytaga@gmail.com>
2023-05-24 14:53:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/blockchain/MyCaptureActivity.java
@@ -0,0 +1,8 @@
+package com.example.blockchain;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class MyCaptureActivity extends CaptureActivity {
+    // 空类即可，继承自CaptureActivity
+}
+
